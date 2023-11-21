7961b286e921c6ccd75125bb4c813bcf0b681a4e
Lalitha <lalithaediga66@gmail.com>
2023-11-21 10:02:55 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def lalitha(request):
+    return render(request,'lalitha.html')
+def vijju(request):
+    return render(request,'vijju.html')
+def vijju2(request):
+    return render(request,'vijju2.html')
+def lalitha2(request):
+    return render(request,'lalitha2.html')
